0951880c9db36e65c3135c9b37394c8ae3ad1358
Aravindan R R <93966376+AravindanRR@users.noreply.github.com>
2023-08-03 21:53:23 +0530

    Initial Changes

---
+++ PhoneInfo.java
@@ -0,0 +1,30 @@
+package org.phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+		// TODO Auto-generated method stub
+System.out.println("Mac");
+	}private void phoneNumber() {
+		// TODO Auto-generated method stub
+		System.out.println("5484321154");
+	}private void phoneId() {
+		// TODO Auto-generated method stub
+		System.out.println("9795");
+	}private void camera() {
+		// TODO Auto-generated method stub
+		System.out.println("4");
+	}private void osName() {
+		// TODO Auto-generated method stub
+		System.out.println("ios");
+	}
+	public static void main(String[] args) {
+		PhoneInfo in = new PhoneInfo();
+		in.camera();
+		in.osName();
+		in.phoneId();
+		in.phoneName();
+		in.phoneNumber();
+	}
+	
+
+}
